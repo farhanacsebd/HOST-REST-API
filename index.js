bbce62406b8389d1fc0ae7352ccb9c0352370552
farhanacsebd <farhana.cse.bd@gmail.com>
2023-09-08 23:53:59 +0600

    lets create server

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 const express = require("express");
 const app = express();
 
+
+PORT = process.env.PORT || 5000;
+
+
+app.get("/", (req,res) =>{
+    res.send("Hi I am live")
+})
+
+
+const start = async () => {
+    try {
+        app.listen(PORT,() =>{
+            console.log(`Server is running at http://localhost:${PORT}`)
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
