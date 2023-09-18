18c0029151b74a0831385c74a62bed80fda132b1
farhanacsebd <farhana.cse.bd@gmail.com>
2023-09-18 23:35:00 +0600

    Filter in API

--- controllers/allProducts.js
+++ controllers/allProducts.js
@@ -2,7 +2,15 @@ const Product = require("../models/product");
 
 
 const getallProducts = async(req,res) => {
-    const myData = await Product.find(req.query)
+    
+    const {company} = req.query;
+    const queryObject = {};
+
+    if(company) {
+        queryObject.company = company;
+    }
+    
+    const myData = await Product.find(queryObject)
     res.status(200).json({myData})
 }
 
